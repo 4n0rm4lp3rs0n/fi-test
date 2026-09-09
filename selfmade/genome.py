3045e19b2380819e023d18abc376bb49b6671794
4n0rm4lp3rs0n <dophuvinh2605@gmail.com>
2026-09-09 18:20:18 +0700

    laptop push

--- selfmade/genome.py
+++ selfmade/genome.py
@@ -666,6 +666,21 @@ class Evaluator:
             return data
         if self.method == "nasbench201":
             return "breh"
+        
+    def nb201parser(inp_layers : list):
+        # exp list : [1,3,2,3,1]
+        op_dict = {0 : "none", 1 : "skip_connect", 2 : "nor_conv_1x1", 3 : "nor_conv_3x3", 4 : "avg_pool_3x3"}
+        idx = 0
+        query = []
+        for layer in range(3):
+            qp = []
+            for i in range(layer):
+                op = op_dict[inp_layers[idx]]
+                qp.append(f"{op}~{i}")
+                idx += 1
+            query.append(qp)
+                
+        return query
 
 # HELPER FUNCTIONS
 
--- selfmade/test3.py
+++ selfmade/test3.py
@@ -1,31 +1,32 @@
 from nb201.nas_201_api import NASBench201API as API
 # Create an API without the verbose log
 # api = API('./nb201/NAS-Bench-201-v1_1-096897.pth', verbose=False)
-api = API('./nb201/NAS-Bench-201-v1_1-096897.pth')
+api = API('./datas/NAS-Bench-201-v1_1-096897.pth')
+print("api created")
 
 num = len(api)
 for i, arch_str in enumerate(api):
   print ('{:5d}/{:5d} : {:}'.format(i, len(api), arch_str))
   
-# show all information for a specific architecture
-api.show(1)
-api.show(2)
+# # show all information for a specific architecture
+# api.show(1)
+# api.show(2)
 
 # show the mean loss and accuracy of an architecture
-info = api.query_meta_info_by_index(1)  # This is an instance of `ArchResults`
-res_metrics = info.get_metrics('cifar10', 'train') # This is a dict with metric names as keys
-cost_metrics = info.get_comput_costs('cifar100') # This is a dict with metric names as keys, e.g., flops, params, latency
+# info = api.query_meta_info_by_index(1)  # This is an instance of `ArchResults`
+# res_metrics = info.get_metrics('cifar10', 'train') # This is a dict with metric names as keys
+# cost_metrics = info.get_comput_costs('cifar100') # This is a dict with metric names as keys, e.g., flops, params, latency
 
 # get the detailed information
-results = api.query_by_index(1, 'cifar100') # a dict of all trials for 1st net on cifar100, where the key is the seed
-print ('There are {:} trials for this architecture [{:}] on cifar100'.format(len(results), api[1]))
-for seed, result in results.items():
-  print ('Latency : {:}'.format(result.get_latency()))
-  print ('Train Info : {:}'.format(result.get_train()))
-  print ('Valid Info : {:}'.format(result.get_eval('x-valid')))
-  print ('Test  Info : {:}'.format(result.get_eval('x-test')))
-  # for the metric after a specific epoch
-  print ('Train Info [10-th epoch] : {:}'.format(result.get_train(10)))
+# results = api.query_by_index(1, 'cifar100') # a dict of all trials for 1st net on cifar100, where the key is the seed
+# index = api.query_index_by_arch('|nor_conv_3x3~0|+|nor_conv_3x3~0|avg_pool_3x3~1|+|skip_connect~0|nor_conv_3x3~1|skip_connect~2|')
+# print ('There are {:} trials for this architecture [{:}] on cifar100'.format(len(results), api[1]))
+# for seed, result in results.items():
+#   print ('Latency : {:}'.format(result.get_latency()))
+#   print ('Train Info : {:}'.format(result.get_train()))
+#   print ('Valid Info : {:}'.format(result.get_eval('x-valid')))
+#   print ('Test  Info : {:}'.format(result.get_eval('x-test')))
+#   # for the metric after a specific epoch
+#   print ('Train Info [10-th epoch] : {:}'.format(result.get_train(10)))
   
-index = api.query_index_by_arch('|nor_conv_3x3~0|+|nor_conv_3x3~0|avg_pool_3x3~1|+|skip_connect~0|nor_conv_3x3~1|skip_connect~2|')
-api.show(index)
+# api.show(index)
